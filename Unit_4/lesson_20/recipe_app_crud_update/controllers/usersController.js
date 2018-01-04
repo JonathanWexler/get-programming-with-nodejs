6c3a258781f0c0a22aff8e978abc12d86d4d4389
Jon Wexler <jwexman@gmail.com>
2018-01-04 03:05:10 -0500

    lesson 20 code

---
+++ Unit_4/lesson_20/recipe_app_crud_update/controllers/usersController.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const User = require('../models/user');
+
+module.exports = {
+  index: (req, res, next) => {
+    User.find()
+    .then(users => {
+      res.locals.users = users;
+      next();
+    })
+    .catch( error =>{
+      console.log(`Error fetching users: ${error.message}`);
+      next(error);
+    });
+  },
+  indexView: (req, res) => {
+    res.render('users/index');
+  },
+
+  new: (req, res) => {
+    res.render('users/new');
+  },
+
+  create: (req, res, next) => {
+    console.log(req.body)
+
+    var userParams = {name: {first: req.body.first, last: req.body.last}, email: req.body.email, password: req.body.password, zipCode: req.body.zipCode};
+    User.create(userParams)
+    .then(user => {
+      res.locals.user = user;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error saving user: ${error.message}`)
+      next(error);
+    });
+  },
+
+  createView: (req, res) => {
+    if (res.locals.user) res.redirect('/users');
+    else res.redirect('users/new');
+  },
+
+  show: (req, res, next) => {
+    var userId = req.params.id;
+    User.findById(userId)
+    .then(user => {
+      res.locals.user = user;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error fetching user by ID: ${error.message}`)
+      next(error);
+    });
+  },
+
+  showView: (req, res) => {
+    res.render('users/show');
+  }
+
+};
