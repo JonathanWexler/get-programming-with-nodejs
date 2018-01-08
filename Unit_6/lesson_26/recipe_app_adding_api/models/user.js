2ac6760f38d863b26972b4ac1c4070900a6399f0
Jon Wexler <jwexman@gmail.com>
2018-01-07 22:49:52 -0500

    code for lesson 26

---
+++ Unit_6/lesson_26/recipe_app_adding_api/models/user.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const mongoose = require('mongoose'),
+Schema = require('mongoose').Schema,
+Subscriber = require('./subscriber'),
+bcrypt = require('bcrypt'),
+passportLocalMongoose = require('passport-local-mongoose');
+
+var userSchema = new Schema({
+  name: {
+    first: {
+      type: String,
+      trim: true
+    },
+    last: {
+      type: String,
+      trim: true
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  zipCode:  {
+    type: Number,
+    min: [1000, 'Zip code too short'],
+    max: 99999
+  },
+  subscribedAccount: {type: Schema.Types.ObjectId, ref: 'Subscriber'},
+  courses: [{type: Schema.Types.ObjectId, ref: 'Course'}],
+},
+{
+  timestamps: true
+});
+
+userSchema.virtual('fullName').get(function(){
+  return `${this.name.first} ${this.name.last}`;
+});
+
+userSchema.pre('save', function (next) {
+  var user = this;
+  if (user.subscribedAccount === undefined) {
+    Subscriber.findOne({email: user.email})
+    .then(subscriber => {
+      user.subscribedAccount = subscriber;
+      next();
+    })
+    .catch(e => {
+      console.log(`Error in connecting subscriber: ${e.message}`);
+      next(e);
+    });
+  } else {
+    next();
+  }
+});
+
+// userSchema.pre('save', function(next) {
+//   let user = this;
+//
+//   bcrypt.hash(user.password, 10).then(hash => {
+//     user.password = hash;
+//     next();
+//   })
+//   .catch(error => {
+//     console.log(`Error in encrypting password: ${e.message}`);
+//     next(error);
+//   });
+// });
+
+// userSchema.methods.passwordComparison = function(inputPassword){
+//   let user = this;
+//   return bcrypt.compare(inputPassword, user.password);
+// };
+
+userSchema.plugin(passportLocalMongoose, {usernameField: 'email'});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ Unit_6/lesson_26/recipe_app_adding_api/public/js/recipeApp.js
@@ -0,0 +1,14 @@
+$(document).ready(()=>{
+  $("#modal-button").click(()=>{
+    $(".modal-body").html('');
+    $.get("/courses?format=json", (data) => {
+      data.forEach((course) => {
+        console.log(course.title)
+        $(".modal-body").append(`<div>
+            <span>${course.title}</span>
+            <div>${course.description}</div>
+          </div>`);
+      });
+    })
+  });
+});
