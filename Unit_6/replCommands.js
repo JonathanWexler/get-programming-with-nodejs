404c7b8ea3c9b5fad213919fb77483ec4d9061d1
Jon Wexler <jwexman@gmail.com>
2018-01-11 02:19:21 -0500

    removed old code

--- Unit_6/replCommands.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/csa_app_db');
-mongoose.Promise = global.Promise;
-
-
-const Course = require('../models/course');
-const User = require('../models/user');
