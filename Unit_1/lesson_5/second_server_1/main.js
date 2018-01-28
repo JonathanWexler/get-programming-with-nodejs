920cefa03ba71c409aa3fae0afd470cad5e971d8
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-01-28 14:28:11 +0530

    Improvements and refactoring.

--- Unit_1/lesson_5/second_server_1/main.js
+++ Unit_1/lesson_5/second_server_1/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
-const http = require('http'),
+// This method given an object, returns a nicely formatted JSON string representation.
+let getJSONString = (obj) => {
+  return JSON.stringify(obj, null, 2);
+}
+
+const port = 3000,
+  http = require('http'),
   app = http.createServer();
 
-app.on('request',(req, res) => {
-  res.writeHead(200, {"Content-Type": "text/html"});
+app.on('request', (req, res) => {
+  console.log(`Method: ${getJSONString(req.method)}`);
+  console.log(`URL: ${getJSONString(req.url)}`);
+  console.log(`Headers: ${getJSONString(req.headers)}`);
+
+  res.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+
   res.end("<h1>This will show on the screen.</h1>");
 });
 
-app.listen(3000);
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
