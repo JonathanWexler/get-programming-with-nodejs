5b1aa09a444cfa38709e871065ff81234c0c09cd
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-01-28 14:50:29 +0530

    Added code to support the example from previous listing. Second_Server2.

--- Unit_1/lesson_5/second_server_3/main.js
+++ Unit_1/lesson_5/second_server_3/main.js
@@ -1,22 +1,35 @@
 'use strict';
 
-const http = require('http'),
+// This method given an object, returns a nicely formatted JSON string representation.
+let getJSONString = (obj) => {
+  return JSON.stringify(obj, null, 2);
+}
+
+const port = 3000,
+  http = require('http'),
   app = http.createServer();
 
-app.on('request',(req, res) => {
+app.on('request', (req, res) => {
   var body = [];
-  req.on('data',(bodyData) => {
+  req.on('data', (bodyData) => {
     body.push(bodyData);
   });
+
   req.on('end', () => {
     body = Buffer.concat(body).toString();
     console.log(`Request Body Contents: ${body}`);
   });
-  console.log(req.method);
-  console.log(req.url);
-  console.log(req.headers);
-  res.writeHead(200, {"Content-Type": "text/html"});
+
+  console.log(`Method: ${getJSONString(req.method)}`);
+  console.log(`URL: ${getJSONString(req.url)}`);
+  console.log(`Headers: ${getJSONString(req.headers)}`);
+
+  res.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+
   res.end("<h1>This will show on the screen.</h1>");
 });
 
-app.listen(3000);
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
