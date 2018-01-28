5a37c50702131021d6c47b54c1c167144299f0f2
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-01-28 15:08:00 +0530

    Using a map object to route across URLs.

--- Unit_1/lesson_5/simple_routes/main.js
+++ Unit_1/lesson_5/simple_routes/main.js
@@ -1,13 +1,25 @@
 'use strict';
 
-const http = require('http'),
-  app = http.createServer((req, res) =>{
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  if (req.url === '/info') {
-    res.end('<h1>Info Page</h1>')
-  } else if (req.url === '/contact') {
-    res.end('<h1>Contact Us</h1>')
-  } else {
-    res.end('<h1>Welcome!</h1>')
-  }
-}).listen(3000);
+// This serves as the dictionary of all responses.
+// URL is the key and the sample HTML response in the value.
+const routeResponseMap = {
+  '/info': '<h1>Info Page</h1>',
+  '/contact': '<h1>Contact Us</h1>'
+};
+
+const port = 3000,
+  http = require('http'),
+  app = http.createServer((req, res) => {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+
+    if (routeResponseMap[req.url]) {
+      res.end(routeResponseMap[req.url]);
+    } else {
+      res.end('<h1>Welcome!</h1>');
+    }
+  }).listen(port);
+
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
