c777071d72cef42f43628b3b87b0ebb78291ae23
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-01-28 12:04:23 +0530

    Updated code to latest standards.

--- Unit_1/lesson_4/simple_server/main.js
+++ Unit_1/lesson_4/simple_server/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
+const port = 3000;
 const http = require('http'),
   app = http.createServer((request, response) => {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<h1>Hello, Universe!</h1>");
-  response.end();
-});
+    console.log("Received an incoming request!");
+    response.writeHead(200, {
+      "Content-Type": "text/html"
+    });
 
-app.listen(3000);
+    let responseMessage = "<h1>Hello, Universe!</h1>";
+    response.write(responseMessage);
+    response.end();
+    console.log(`Sent a response : ${responseMessage}`);
+  });
+
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
