dd35d99214edc6c2add57f63016336f201ba2fff
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-01-28 15:21:33 +0530

    Adding routes is easy!

--- Unit_1/lesson_5/simple_routes/main.js
+++ Unit_1/lesson_5/simple_routes/main.js
@@ -4,7 +4,10 @@
 // URL is the key and the sample HTML response in the value.
 const routeResponseMap = {
   '/info': '<h1>Info Page</h1>',
-  '/contact': '<h1>Contact Us</h1>'
+  '/contact': '<h1>Contact Us</h1>',
+  '/about': '<h1>Learn More About Us.</h1>',
+  '/hello': '<h1>Say hello by emailing us here</h1>',
+  '/error': '<h1>Sorry the page you are looking for is not here.</h1>'
 };
 
 const port = 3000,
