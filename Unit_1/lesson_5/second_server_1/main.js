ab699cc14c5ebf2749ef047a92080a8840393fa7
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-01-28 14:32:12 +0530

    Migrated code to second_server 2 to match the book.

--- Unit_1/lesson_5/second_server_1/main.js
+++ Unit_1/lesson_5/second_server_1/main.js
@@ -1,19 +1,10 @@
 'use strict';
 
-// This method given an object, returns a nicely formatted JSON string representation.
-let getJSONString = (obj) => {
-  return JSON.stringify(obj, null, 2);
-}
-
 const port = 3000,
   http = require('http'),
   app = http.createServer();
 
 app.on('request', (req, res) => {
-  console.log(`Method: ${getJSONString(req.method)}`);
-  console.log(`URL: ${getJSONString(req.url)}`);
-  console.log(`Headers: ${getJSONString(req.headers)}`);
-
   res.writeHead(200, {
     "Content-Type": "text/html"
   });
