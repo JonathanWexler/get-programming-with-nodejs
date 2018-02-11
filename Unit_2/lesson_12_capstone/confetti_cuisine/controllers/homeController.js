4be4dae77d026a1038e3d6bc4dd7323d589eea46
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-02-11 13:26:12 +0530

    Code changes for lessson 12.

--- Unit_2/lesson_12_capstone/confetti_cuisine/controllers/homeController.js
+++ Unit_2/lesson_12_capstone/confetti_cuisine/controllers/homeController.js
@@ -1,10 +1,20 @@
 'use strict';
 
-var courses =[{title: "Event Driven Cakes", cost: 50}, {title: "Asynchronous Artichoke", cost: 25}, {title: "Object Oriented Orange Juice", cost: 10}];
+var courses = [{
+  title: "Event Driven Cakes",
+  cost: 50
+}, {
+  title: "Asynchronous Artichoke",
+  cost: 25
+}, {
+  title: "Object Oriented Orange Juice",
+  cost: 10
+}];
 
 exports.showCourses = (req, res) => {
-  res.render('courses', {offeredCourses:
-  courses});
+  res.render('courses', {
+    offeredCourses: courses
+  });
 };
 
 exports.showSignUp = (req, res) => {
