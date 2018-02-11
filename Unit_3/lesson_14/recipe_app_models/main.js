09346e76ed30323f35cd3552ede76de22dbdd662
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-02-11 15:43:50 +0530

    Additional files ffor lesson 14. Review.

--- Unit_3/lesson_14/recipe_app_models/main.js
+++ Unit_3/lesson_14/recipe_app_models/main.js
@@ -12,13 +12,18 @@ const express = require('express'),
 mongoose.connect('mongodb://localhost/recipe_db');
 var db = mongoose.connection;
 
-let subscriber1  = new Subscriber({name: "Jon Wexler", email: "jon@jonwexler.com"});
-subscriber1.save((error, savedDocument, next) =>{
+let subscriber1 = new Subscriber({
+  name: "Jon Wexler",
+  email: "jon@jonwexler.com"
+});
+subscriber1.save((error, savedDocument, next) => {
   if (error) next(error);
   console.log(savedDocument);
 });
 
-var myQuery = Subscriber.findOne({name: "Jon Wexler"}).where('email', /wexler/);
+var myQuery = Subscriber.findOne({
+  name: "Jon Wexler"
+}).where('email', /wexler/);
 myQuery.exec((error, data) => {
   if (data) console.log(data.name);
 });
@@ -33,16 +38,18 @@ app.set('view engine', 'ejs');
 app.use(layouts);
 app.use(express.static(`${__dirname}/public`));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/courses', homeController.showCourses );
-app.get('/contact', homeController.showSignUp );
-app.post('/sign-up',homeController.postedSignUpForm );
+app.get('/courses', homeController.showCourses);
+app.get('/contact', homeController.showSignUp);
+app.post('/sign-up', homeController.postedSignUpForm);
 app.post('/contact', homeController.postedContactForm);
 
 // Error middleware
