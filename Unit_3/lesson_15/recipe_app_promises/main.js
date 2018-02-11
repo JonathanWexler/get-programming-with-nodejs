d028a478923ce12c1526f456097d868f0c629ecf
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-02-11 16:17:15 +0530

    Lesson 15 - Refactoring All Lessons - * Validation for ZIP code in UI * Changed input type to email for email field in subcribers page.

--- Unit_3/lesson_15/recipe_app_promises/main.js
+++ Unit_3/lesson_15/recipe_app_promises/main.js
@@ -18,13 +18,17 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/recipe_db');
 var db = mongoose.connection;
 
+// TODO: Have a separate module seed.js and move this code there! 
+// You can include all the dummy data in seed.js and initialize the database using the seed module.
 // let subscriber1  = new Subscriber({name: "Jon Wexler", email: "jon@jonwexler.com"});
 // subscriber1.save((error, savedDocument, next) =>{
 //   if (error) next(error);
 //   console.log(savedDocument);
 // });
 
-var myQuery = Subscriber.findOne({name: "Jon Wexler"}).where('email', /wexler/);
+var myQuery = Subscriber.findOne({
+  name: "Jon Wexler"
+}).where('email', /wexler/);
 myQuery.exec((error, data) => {
   if (data) console.log(data.name);
 });
@@ -39,7 +43,9 @@ app.set('view engine', 'ejs');
 app.use(layouts);
 app.use(express.static(`${__dirname}/public`));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
@@ -50,9 +56,9 @@ app.get('/subscribers', subscribersController.getAllSubscribers);
 app.get('/subscribe', subscribersController.getSubscriptionPage);
 app.post('/subscribe', subscribersController.saveSubscriber);
 
-app.get('/courses', homeController.showCourses );
-app.get('/contact', homeController.showSignUp );
-app.post('/sign-up',homeController.postedSignUpForm );
+app.get('/courses', homeController.showCourses);
+app.get('/contact', homeController.showSignUp);
+app.post('/sign-up', homeController.postedSignUpForm);
 app.post('/contact', homeController.postedContactForm);
 
 // Error middleware
