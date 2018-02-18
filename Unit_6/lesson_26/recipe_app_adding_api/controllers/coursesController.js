e4c1d51d41c9cc0d5f7104b90cd0fc9ccd9776d0
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-02-18 18:41:58 +0530

    Lesson 26 - formatting and refactoring.

--- Unit_6/lesson_26/recipe_app_adding_api/controllers/coursesController.js
+++ Unit_6/lesson_26/recipe_app_adding_api/controllers/coursesController.js
@@ -5,14 +5,14 @@ const Course = require('../models/course');
 module.exports = {
   index: (req, res, next) => {
     Course.find()
-    .then(courses => {
-      res.locals.courses = courses;
-      next();
-    })
-    .catch( error =>{
-      console.log(`Error fetching courses: ${error.message}`);
-      next(error);
-    });
+      .then(courses => {
+        res.locals.courses = courses;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching courses: ${error.message}`);
+        next(error);
+      });
   },
   indexView: (req, res, next) => {
     if (req.query.format === 'json') {
@@ -27,20 +27,25 @@ module.exports = {
   },
 
   create: (req, res, next) => {
-    var courseParams = {title: req.body.title, description: req.body.description, items: [req.body.items.split(",")], zipCode: req.body.zipCode};
+    var courseParams = {
+      title: req.body.title,
+      description: req.body.description,
+      items: [req.body.items.split(",")],
+      zipCode: req.body.zipCode
+    };
     Course.create(courseParams)
-    .then(course => {
-      res.locals.redirect = '/courses';
-      req.flash('success', `${course.title}'s account created successfully!`);
-      res.locals.course = course;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error saving course: ${error.message}`);
-      req.flash('error', `Failed to create course because: ${error.message}.`);
-      res.locals.redirect = `/courses/new`;
-      next();
-    });
+      .then(course => {
+        res.locals.redirect = '/courses';
+        req.flash('success', `${course.title}'s account created successfully!`);
+        res.locals.course = course;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error saving course: ${error.message}`);
+        req.flash('error', `Failed to create course because: ${error.message}.`);
+        res.locals.redirect = `/courses/new`;
+        next();
+      });
   },
 
   redirectView: (req, res, next) => {
@@ -52,14 +57,14 @@ module.exports = {
   show: (req, res, next) => {
     var courseId = req.params.id;
     Course.findById(courseId)
-    .then(course => {
-      res.locals.course = course;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error fetching course by ID: ${error.message}`)
-      next(error);
-    });
+      .then(course => {
+        res.locals.course = course;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching course by ID: ${error.message}`)
+        next(error);
+      });
   },
 
   showView: (req, res) => {
@@ -69,45 +74,54 @@ module.exports = {
   edit: (req, res, next) => {
     var courseId = req.params.id;
     Course.findById(courseId)
-    .then(course => {
-      res.render('courses/edit', {course: course});
-    })
-    .catch(error => {
-      console.log(`Error fetching course by ID: ${error.message}`);
-      next(error);
-    });
+      .then(course => {
+        res.render('courses/edit', {
+          course: course
+        });
+      })
+      .catch(error => {
+        console.log(`Error fetching course by ID: ${error.message}`);
+        next(error);
+      });
   },
 
   update: (req, res, next) => {
     var courseId = req.params.id,
-    courseParams = {title: req.body.title, description: req.body.description, items: [req.body.items.split(",")], zipCode: req.body.zipCode};
+      courseParams = {
+        title: req.body.title,
+        description: req.body.description,
+        items: [req.body.items.split(",")],
+        zipCode: req.body.zipCode
+      };
 
-    Course.findByIdAndUpdate(courseId, { $set: courseParams })
-    .then(course => {
-      res.locals.redirect = `/courses/${courseId}`;
-      req.flash('success', `${course.title}'s account updated successfully!`);
-      res.locals.course = course;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error updating course by ID: ${error.message}`);
-      req.flash('error', `Failed to update course because: ${error.message}.`);
-      res.locals.redirect = `/courses/${courseId}/edit`;
-      next();
-    });
+    Course.findByIdAndUpdate(courseId, {
+        $set: courseParams
+      })
+      .then(course => {
+        res.locals.redirect = `/courses/${courseId}`;
+        req.flash('success', `${course.title}'s account updated successfully!`);
+        res.locals.course = course;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error updating course by ID: ${error.message}`);
+        req.flash('error', `Failed to update course because: ${error.message}.`);
+        res.locals.redirect = `/courses/${courseId}/edit`;
+        next();
+      });
   },
 
   delete: (req, res, next) => {
     var courseId = req.params.id;
     Course.findByIdAndRemove(courseId)
-    .then(course => {
-      res.locals.redirect = '/courses';
-      req.flash('success', `${course.title} deleted successfully!`);
-      next();
-    })
-    .catch(error => {
-      console.log(`Error deleting course by ID: ${error.message}`);
-      next();
-    });
+      .then(course => {
+        res.locals.redirect = '/courses';
+        req.flash('success', `${course.title} deleted successfully!`);
+        next();
+      })
+      .catch(error => {
+        console.log(`Error deleting course by ID: ${error.message}`);
+        next();
+      });
   }
 };
--- Unit_6/lesson_26/recipe_app_adding_api/models/user.js
+++ Unit_6/lesson_26/recipe_app_adding_api/models/user.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const mongoose = require('mongoose'),
-Schema = require('mongoose').Schema,
-Subscriber = require('./subscriber'),
-bcrypt = require('bcrypt'),
-passportLocalMongoose = require('passport-local-mongoose');
+  Schema = require('mongoose').Schema,
+  Subscriber = require('./subscriber'),
+  bcrypt = require('bcrypt'),
+  passportLocalMongoose = require('passport-local-mongoose');
 
 var userSchema = new Schema({
   name: {
@@ -23,34 +23,41 @@ var userSchema = new Schema({
     lowercase: true,
     unique: true
   },
-  zipCode:  {
+  zipCode: {
     type: Number,
     min: [10000, 'Zip code too short'],
     max: 99999
   },
-  subscribedAccount: {type: Schema.Types.ObjectId, ref: 'Subscriber'},
-  courses: [{type: Schema.Types.ObjectId, ref: 'Course'}],
-},
-{
+  subscribedAccount: {
+    type: Schema.Types.ObjectId,
+    ref: 'Subscriber'
+  },
+  courses: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Course'
+  }],
+}, {
   timestamps: true
 });
 
-userSchema.virtual('fullName').get(function(){
+userSchema.virtual('fullName').get(function () {
   return `${this.name.first} ${this.name.last}`;
 });
 
 userSchema.pre('save', function (next) {
   var user = this;
   if (user.subscribedAccount === undefined) {
-    Subscriber.findOne({email: user.email})
-    .then(subscriber => {
-      user.subscribedAccount = subscriber;
-      next();
-    })
-    .catch(e => {
-      console.log(`Error in connecting subscriber: ${e.message}`);
-      next(e);
-    });
+    Subscriber.findOne({
+        email: user.email
+      })
+      .then(subscriber => {
+        user.subscribedAccount = subscriber;
+        next();
+      })
+      .catch(e => {
+        console.log(`Error in connecting subscriber: ${e.message}`);
+        next(e);
+      });
   } else {
     next();
   }
@@ -74,6 +81,8 @@ userSchema.pre('save', function (next) {
 //   return bcrypt.compare(inputPassword, user.password);
 // };
 
-userSchema.plugin(passportLocalMongoose, {usernameField: 'email'});
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: 'email'
+});
 
 module.exports = mongoose.model('User', userSchema);
