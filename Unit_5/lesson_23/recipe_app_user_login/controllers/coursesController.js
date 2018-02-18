c530022e42ff5a6775c96323dd8a4a5b3028d901
SRIHARI-L470\Srihari <srihari.sridharan@live.com>
2018-02-18 17:04:39 +0530

    Recipe app user login - code formatting and minor changes. Reported the issue on subscribedAccount being null in certain cases. If that is a bug all across, needs to fixed, so that we have the linking setup properly.

--- Unit_5/lesson_23/recipe_app_user_login/controllers/coursesController.js
+++ Unit_5/lesson_23/recipe_app_user_login/controllers/coursesController.js
@@ -5,14 +5,14 @@ const Course = require('../models/course');
 module.exports = {
   index: (req, res, next) => {
     Course.find()
-    .then(courses => {
-      res.locals.courses = courses;
-      next();
-    })
-    .catch( error =>{
-      console.log(`Error fetching courses: ${error.message}`);
-      next(error);
-    });
+      .then(courses => {
+        res.locals.courses = courses;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching courses: ${error.message}`);
+        next(error);
+      });
   },
   indexView: (req, res, next) => {
     res.render('courses/index');
@@ -23,20 +23,25 @@ module.exports = {
   },
 
   create: (req, res, next) => {
-    var courseParams = {title: req.body.title, description: req.body.description, items: [req.body.items.split(",")], zipCode: req.body.zipCode};
+    var courseParams = {
+      title: req.body.title,
+      description: req.body.description,
+      items: [req.body.items.split(",")],
+      zipCode: req.body.zipCode
+    };
     Course.create(courseParams)
-    .then(course => {
-      res.locals.redirect = '/courses';
-      req.flash('success', `${course.title}'s account created successfully!`);
-      res.locals.course = course;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error saving course: ${error.message}`);
-      req.flash('error', `Failed to create course because: ${error.message}.`);
-      res.locals.redirect = `/courses/new`;
-      next();
-    });
+      .then(course => {
+        res.locals.redirect = '/courses';
+        req.flash('success', `${course.title}'s account created successfully!`);
+        res.locals.course = course;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error saving course: ${error.message}`);
+        req.flash('error', `Failed to create course because: ${error.message}.`);
+        res.locals.redirect = `/courses/new`;
+        next();
+      });
   },
 
   redirectView: (req, res, next) => {
@@ -48,14 +53,14 @@ module.exports = {
   show: (req, res, next) => {
     var courseId = req.params.id;
     Course.findById(courseId)
-    .then(course => {
-      res.locals.course = course;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error fetching course by ID: ${error.message}`)
-      next(error);
-    });
+      .then(course => {
+        res.locals.course = course;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching course by ID: ${error.message}`)
+        next(error);
+      });
   },
 
   showView: (req, res) => {
@@ -65,45 +70,54 @@ module.exports = {
   edit: (req, res, next) => {
     var courseId = req.params.id;
     Course.findById(courseId)
-    .then(course => {
-      res.render('courses/edit', {course: course});
-    })
-    .catch(error => {
-      console.log(`Error fetching course by ID: ${error.message}`);
-      next(error);
-    });
+      .then(course => {
+        res.render('courses/edit', {
+          course: course
+        });
+      })
+      .catch(error => {
+        console.log(`Error fetching course by ID: ${error.message}`);
+        next(error);
+      });
   },
 
   update: (req, res, next) => {
     var courseId = req.params.id,
-    courseParams = {title: req.body.title, description: req.body.description, items: [req.body.items.split(",")], zipCode: req.body.zipCode};
+      courseParams = {
+        title: req.body.title,
+        description: req.body.description,
+        items: [req.body.items.split(",")],
+        zipCode: req.body.zipCode
+      };
 
-    Course.findByIdAndUpdate(courseId, { $set: courseParams })
-    .then(course => {
-      res.locals.redirect = `/courses/${courseId}`;
-      req.flash('success', `${course.title}'s account updated successfully!`);
-      res.locals.course = course;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error updating course by ID: ${error.message}`);
-      req.flash('error', `Failed to update course because: ${error.message}.`);
-      res.locals.redirect = `/courses/${courseId}/edit`;
-      next();
-    });
+    Course.findByIdAndUpdate(courseId, {
+        $set: courseParams
+      })
+      .then(course => {
+        res.locals.redirect = `/courses/${courseId}`;
+        req.flash('success', `${course.title}'s account updated successfully!`);
+        res.locals.course = course;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error updating course by ID: ${error.message}`);
+        req.flash('error', `Failed to update course because: ${error.message}.`);
+        res.locals.redirect = `/courses/${courseId}/edit`;
+        next();
+      });
   },
 
   delete: (req, res, next) => {
     var courseId = req.params.id;
     Course.findByIdAndRemove(courseId)
-    .then(course => {
-      res.locals.redirect = '/courses';
-      req.flash('success', `${course.title} deleted successfully!`);
-      next();
-    })
-    .catch(error => {
-      console.log(`Error deleting course by ID: ${error.message}`);
-      next();
-    });
+      .then(course => {
+        res.locals.redirect = '/courses';
+        req.flash('success', `${course.title} deleted successfully!`);
+        next();
+      })
+      .catch(error => {
+        console.log(`Error deleting course by ID: ${error.message}`);
+        next();
+      });
   }
 };
--- Unit_5/lesson_23/recipe_app_user_login/controllers/usersController.js
+++ Unit_5/lesson_23/recipe_app_user_login/controllers/usersController.js
@@ -5,14 +5,14 @@ const User = require('../models/user');
 module.exports = {
   index: (req, res, next) => {
     User.find()
-    .then(users => {
-      res.locals.users = users;
-      next();
-    })
-    .catch( error =>{
-      console.log(`Error fetching users: ${error.message}`);
-      next(error);
-    });
+      .then(users => {
+        res.locals.users = users;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching users: ${error.message}`);
+        next(error);
+      });
   },
   indexView: (req, res) => {
     res.render('users/index');
@@ -24,20 +24,28 @@ module.exports = {
 
   create: (req, res, next) => {
 
-    var userParams = {name: {first: req.body.first, last: req.body.last}, email: req.body.email, password: req.body.password, zipCode: req.body.zipCode};
+    var userParams = {
+      name: {
+        first: req.body.first,
+        last: req.body.last
+      },
+      email: req.body.email,
+      password: req.body.password,
+      zipCode: req.body.zipCode
+    };
     User.create(userParams)
-    .then(user => {
-      res.locals.redirect = '/users';
-      req.flash('success', `${user.fullName}'s account created successfully!`);
-      res.locals.user = user;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error saving user: ${error.message}`);
-      res.locals.redirect = '/users/new';
-      req.flash('error', `Failed to create user account because: ${error.message}.`);
-      next();
-    });
+      .then(user => {
+        res.locals.redirect = '/users';
+        req.flash('success', `${user.fullName}'s account created successfully!`);
+        res.locals.user = user;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error saving user: ${error.message}`);
+        res.locals.redirect = '/users/new';
+        req.flash('error', `Failed to create user account because: ${error.message}.`);
+        next();
+      });
   },
 
   redirectView: (req, res, next) => {
@@ -49,14 +57,14 @@ module.exports = {
   show: (req, res, next) => {
     var userId = req.params.id;
     User.findById(userId)
-    .then(user => {
-      res.locals.user = user;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error fetching user by ID: ${error.message}`)
-      next(error);
-    });
+      .then(user => {
+        res.locals.user = user;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching user by ID: ${error.message}`)
+        next(error);
+      });
   },
 
   showView: (req, res) => {
@@ -66,70 +74,84 @@ module.exports = {
   edit: (req, res, next) => {
     var userId = req.params.id;
     User.findById(userId)
-    .then(user => {
-      res.render('users/edit', {user: user});
-    })
-    .catch(error => {
-      console.log(`Error fetching user by ID: ${error.message}`);
-      next(error);
-    });
+      .then(user => {
+        res.render('users/edit', {
+          user: user
+        });
+      })
+      .catch(error => {
+        console.log(`Error fetching user by ID: ${error.message}`);
+        next(error);
+      });
   },
 
   update: (req, res, next) => {
     var userId = req.params.id,
-    userParams = {name: {first: req.body.first, last: req.body.last}, email: req.body.email, password: req.body.password, zipCode: req.body.zipCode};
+      userParams = {
+        name: {
+          first: req.body.first,
+          last: req.body.last
+        },
+        email: req.body.email,
+        password: req.body.password,
+        zipCode: req.body.zipCode
+      };
 
-    User.findByIdAndUpdate(userId, { $set: userParams })
-    .then(user => {
-      res.locals.redirect = `/users/${userId}`;
-      req.flash('success', `${user.fullName}'s account updated successfully!`);
-      res.locals.user = user;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error updating user by ID: ${error.message}`);
-      req.flash('error', `Failed to update user account because: ${error.message}.`);
-      res.locals.redirect = `/users/${userId}/edit`;
-      next();
-    });
+    User.findByIdAndUpdate(userId, {
+        $set: userParams
+      })
+      .then(user => {
+        res.locals.redirect = `/users/${userId}`;
+        req.flash('success', `${user.fullName}'s account updated successfully!`);
+        res.locals.user = user;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error updating user by ID: ${error.message}`);
+        req.flash('error', `Failed to update user account because: ${error.message}.`);
+        res.locals.redirect = `/users/${userId}/edit`;
+        next();
+      });
   },
 
   delete: (req, res, next) => {
     var userId = req.params.id;
     User.findByIdAndRemove(userId)
-    .then(user => {
-      res.locals.redirect = '/users';
-      req.flash('success', `${user.fullName}'s account deleted successfully!`);
-      next();
-    })
-    .catch(error => {
-      console.log(`Error deleting user by ID: ${error.message}`);
-      next();
-    });
+      .then(user => {
+        res.locals.redirect = '/users';
+        req.flash('success', `${user.fullName}'s account deleted successfully!`);
+        next();
+      })
+      .catch(error => {
+        console.log(`Error deleting user by ID: ${error.message}`);
+        next();
+      });
   },
 
-login: (req, res) => {
-  res.render('users/login');
-},
+  login: (req, res) => {
+    res.render('users/login');
+  },
 
-authenticate: (req, res, next) => {
-  User.findOne({email: req.body.email})
-  .then(user => {
-    if (user.password === req.body.password){
-      res.locals.redirect = `/users/${user._id}`;
-      req.flash('success', `${user.fullName}'s logged in successfully!`);
-      res.locals.user = user;
-      next();
-    } else {
-      req.flash('error', 'Failed to log in user account.');
-      res.locals.redirect = '/users/login';
-      next();
-    }
-  })
-  .catch(error => {
-    console.log(`Error logging in user: ${error.message}`);
-    next(error);
-  });
-}
+  authenticate: (req, res, next) => {
+    User.findOne({
+        email: req.body.email
+      })
+      .then(user => {
+        if (user.password === req.body.password) {
+          res.locals.redirect = `/users/${user._id}`;
+          req.flash('success', `${user.fullName}'s logged in successfully!`);
+          res.locals.user = user;
+          next();
+        } else {
+          req.flash('error', 'Your account or password is incorrect. Please try again or contact your system administrator!');
+          res.locals.redirect = '/users/login';
+          next();
+        }
+      })
+      .catch(error => {
+        console.log(`Error logging in user: ${error.message}`);
+        next(error);
+      });
+  }
 
 };
