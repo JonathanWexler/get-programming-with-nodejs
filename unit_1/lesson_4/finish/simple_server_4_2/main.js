b48c6b426e1eb12313d174ea6da32ccf93133592
Jon Wexler <jwexman@gmail.com>
2019-01-13 18:26:22 -0500

    [GetProgramming] Lesson 4

---
+++ unit_1/lesson_4/finish/simple_server_4_2/main.js
@@ -0,0 +1,19 @@
+"strict mode";
+
+const port = 3000,
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  app = http.createServer((request, response) => {
+    console.log("Received an incoming request!");
+    response.writeHead(httpStatus.OK, {
+      "Content-Type": "text/html"
+    });
+
+    let responseMessage = "<h1>Hello, Universe!</h1>";
+    response.write(responseMessage);
+    response.end();
+    console.log(`Sent a response : ${responseMessage}`);
+  });
+
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
