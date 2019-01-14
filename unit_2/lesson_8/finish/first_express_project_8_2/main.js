3fbf2f690d2d2635bee54a362651ab83abd5d169
Jon Wexler <jwexman@gmail.com>
2019-01-13 22:53:28 -0500

    [GetProgramming] Lesson 8

---
+++ unit_2/lesson_8/finish/first_express_project_8_2/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const port = 3000,
+  express = require("express"),
+  app = express();
+app
+  .get("/", (req, res) => {
+    res.send("Hello, Universe!");
+  })
+  .listen(port, () => {
+    console.log(
+      `The Express.js server has started and is listening on port number: ${port}`
+    );
+  });
